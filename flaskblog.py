da63ff250bb47aece3ee8253d3ce9e42e7d20901
pranotobudi <pranotobudi.s@gmail.com>
2018-10-16 14:31:04 +0700

    Add template for layout and css file

--- flaskblog.py
+++ flaskblog.py
@@ -1,16 +1,35 @@
 from flask import Flask
+from flask import render_template
+from flask import url_for
+
 app = Flask(__name__)
+
+
+posts = [
+    {
+        'author': 'Pranoto Budi',
+        'title': 'Title 1',
+        'content': "Content 1",
+        'date_posted': 'August 10, 2018',
+    },
+    {
+        'author': 'Dwi Hendro',
+        'title': 'Title 2',
+        'content': "Content 2",
+        'date_posted': 'August 11, 2018',
+    },
+]
 
 
 @app.route("/")
 @app.route('/home')
 def home():
-    return "<h1>Home Page</h1>"
+    return render_template('home.html', posts=posts)
 
 
 @app.route('/about')
 def about():
-    return "<h1>About Page</h1>"
+    return render_template('about.html', title='About')
 
 
 if __name__ == '__main__':
