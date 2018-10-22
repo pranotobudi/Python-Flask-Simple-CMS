23b8a83761904a0d121ab71aab333be2f566a47b
pranotobudi <pranotobudi.s@gmail.com>
2018-10-22 20:28:15 +0700

    pagination, password reset through email

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -62,3 +62,20 @@ class PostForm(FlaskForm):
     title = StringField('Title', validators=[DataRequired()])
     content = TextAreaField('Content', validators=[DataRequired()])
     submit = SubmitField('Post')
+
+
+class RequestResetForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    submit = SubmitField('Request Password Reset')
+
+    # this is custom function: validate_<field_name_variable>, see variable name above
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is None:
+            raise ValidationError('No account with that email, you must register first!')
+
+
+class ResetPasswordForm(FlaskForm):
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Reset Password')
