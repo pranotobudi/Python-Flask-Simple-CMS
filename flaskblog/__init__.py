a5d03cb8c674a65e6c72cad12e45136ae4736918
pranotobudi <pranotobudi.s@gmail.com>
2018-11-18 14:04:17 +0700

    add pagination, password reset features

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,27 +1,36 @@
-import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
+from flaskblog.config import Config
 
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'a9705bdad25a1836d4c5d849dd028ecb'
-# print(secrets.token_hex(16))
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-db = SQLAlchemy(app)
-bcrypt = Bcrypt(app)
-login_manager = LoginManager(app)
-login_manager.login_view = 'login'
+
+db = SQLAlchemy()
+bcrypt = Bcrypt()
+login_manager = LoginManager()
+login_manager.login_view = 'users.login'
 login_manager.login_message_category = 'info'
-app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
-app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
 # app.config['MAIL_USERNAME'] = 'ultiumlabs@gmail.com'
 # app.config['MAIL_PASSWORD'] = 'passwordultiumlabs'
-mail = Mail(app)
+mail = Mail()
 
 
-from flaskblog import routes
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(Config)
+    db.init_app(app)
+    bcrypt.init_app(app)
+    login_manager.init_app(app)
+    mail.init_app(app)
+    #from flaskblog import routes
+    from flaskblog.users.routes import users
+    from flaskblog.posts.routes import posts
+    from flaskblog.main.routes import main
+    from flaskblog.errors.handlers import errors
+    app.register_blueprint(users)
+    app.register_blueprint(posts)
+    app.register_blueprint(main)
+    app.register_blueprint(errors)
+
+    return app
--- run.py
+++ run.py
@@ -1,4 +1,5 @@
-from flaskblog import app
+from flaskblog import create_app
 
+app = create_app()
 if __name__ == '__main__':
     app.run(debug=True)
